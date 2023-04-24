37445a5869bc584b7a8817325d9e08ff3ebf2e46
Iryna Temchuk <itemchuk98@gmail.com>
2023-04-24 15:26:30 +0300

    Fix div with questions

--- src/App.js
+++ src/App.js
@@ -44,8 +44,6 @@ export default function App() {
 
 	return (
 		<div className='app'>
-			{/* HINT: replace "false" with logic to display the 
-      score when the user has answered all the questions */}
 			{false ? (
 				<div className='score-section'>You scored 1 out of {questions.length}</div>
 			) : (
@@ -54,13 +52,10 @@ export default function App() {
 						<div className='question-count'>
 							<span>Question 1</span>/{questions.length}
 						</div>
-						<div className='question-text'>This is where the question text should go</div>
+						<div className='question-text'>{questions[0].questionText}</div>
 					</div>
 					<div className='answer-section'>
-						<button>Answer 1</button>
-						<button>Answer 2</button>
-						<button>Answer 3</button>
-						<button>Answer 4</button>
+						{questions[0].answerOptions.map((answerOptions)=> <button>{answerOptions.answerText}</button>)}
 					</div>
 				</>
 			)}
