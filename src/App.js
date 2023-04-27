c6f53dbad6e7163175f99f56a7891be38de8a833
Iryna Temchuk <itemchuk98@gmail.com>
2023-04-27 18:27:51 +0300

    Add nextbtn + style, fix correct colors

--- src/App.js
+++ src/App.js
@@ -13,37 +13,37 @@ const App = () => {
 		{
 			questionText: 'What is the capital of France?',
 			answerOptions: [
-				{id: 1, answerText: 'New York', isCorrect: false },
-				{id: 2,  answerText: 'London', isCorrect: false },
-				{id: 3,  answerText: 'Paris', isCorrect: true },
-				{id: 4,  answerText: 'Dublin', isCorrect: false },
+				{ id: 1, answerText: 'New York', isCorrect: false },
+				{ id: 2, answerText: 'London', isCorrect: false },
+				{ id: 3, answerText: 'Paris', isCorrect: true },
+				{ id: 4, answerText: 'Dublin', isCorrect: false },
 			],
 		},
 		{
 			questionText: 'Who is CEO of Tesla?',
 			answerOptions: [
-				{id: 1,  answerText: 'Jeff Bezos', isCorrect: false },
-				{id: 2,  answerText: 'Elon Musk', isCorrect: true },
-				{id: 3,  answerText: 'Bill Gates', isCorrect: false },
-				{id: 4,  answerText: 'Tony Stark', isCorrect: false },
+				{ id: 1, answerText: 'Jeff Bezos', isCorrect: false },
+				{ id: 2, answerText: 'Elon Musk', isCorrect: true },
+				{ id: 3, answerText: 'Bill Gates', isCorrect: false },
+				{ id: 4, answerText: 'Tony Stark', isCorrect: false },
 			],
 		},
 		{
 			questionText: 'The iPhone was created by which company?',
 			answerOptions: [
-				{id: 1,  answerText: 'Apple', isCorrect: true },
-				{id: 2,  answerText: 'Intel', isCorrect: false },
-				{id: 3,  answerText: 'Amazon', isCorrect: false },
-				{id: 4,  answerText: 'Microsoft', isCorrect: false },
+				{ id: 1, answerText: 'Apple', isCorrect: true },
+				{ id: 2, answerText: 'Intel', isCorrect: false },
+				{ id: 3, answerText: 'Amazon', isCorrect: false },
+				{ id: 4, answerText: 'Microsoft', isCorrect: false },
 			],
 		},
 		{
 			questionText: 'How many Harry Potter books are there?',
 			answerOptions: [
-				{id: 1,  answerText: '1', isCorrect: false },
-				{id: 2,  answerText: '4', isCorrect: false },
-				{id: 3,  answerText: '6', isCorrect: false },
-				{id: 4,  answerText: '7', isCorrect: true },
+				{ id: 1, answerText: '1', isCorrect: false },
+				{ id: 2, answerText: '4', isCorrect: false },
+				{ id: 3, answerText: '6', isCorrect: false },
+				{ id: 4, answerText: '7', isCorrect: true },
 			],
 		},
 	]);
@@ -52,10 +52,8 @@ const App = () => {
 		if (isCorrect === true) {
 			document.getElementById(id).style.backgroundColor = "green"
 			setScore(score + 1);
-			alert("This answer is correct!");			
 		} else {
-			document.getElementById(id).style.backgroundColor = "red"		
-			alert("This answer is false!");
+			document.getElementById(id).style.backgroundColor = "red"
 		}
 	}
 
@@ -63,8 +61,8 @@ const App = () => {
 		var collection = document.getElementsByClassName('button-answer')
 		for (let i = 0; i < collection.length; i++) {
 			collection[i].style.backgroundColor = "";
-		  }
-	}	
+		}
+	}
 
 	const next = () => {
 		resetBgColor("")
@@ -96,22 +94,26 @@ const App = () => {
 					</>
 				) : (
 					<>
-						<div className='question-section'>
-							<div className='question-count'>
-								<span>Question {currentQuestion + 1}</span>/{questions.length}
+						
+							<div className='question'>
+								<div className='question-section'>
+									<div className='question-count'>
+										<span>Question {currentQuestion + 1}</span>/{questions.length}
+									</div>
+									<div className='question-text'>{questions[currentQuestion].questionText}</div>
+								</div>
+								<div className='answer-section'>
+									{questions[currentQuestion].answerOptions.map((answerOptions) => (
+										<button
+											id={answerOptions.id}
+											className='button-answer'
+											onClick={() => handleAnswerButtonClick(answerOptions.id, answerOptions.isCorrect)}>{answerOptions.answerText}</button>))}
+								</div>
 							</div>
-							<div className='question-text'>{questions[currentQuestion].questionText}</div>
-						</div>
-						<div className='answer-section'>
-							{questions[currentQuestion].answerOptions.map((answerOptions) => (
-								<button									
-									id={answerOptions.id}
-									className='button-answer'
-									onClick={() => handleAnswerButtonClick(answerOptions.id, answerOptions.isCorrect)}>{answerOptions.answerText}</button>))}
-						</div>
-						<div>
-								<button onClick={() => next()}>Next</button>
-						</div>
+							<div className='nextbtn-'>
+								<button className='nextbtn' onClick={() => next()}>Next</button>
+							</div>
+						
 					</>
 				)}
 			</div>
